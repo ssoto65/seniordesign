b7e55cfeb4971f55f86811aaefb459c4d9d4d3c5
Nick Poindexter <nickjpoindexter@gmail.com>
2019-01-11 15:38:51 -0500

    updated pin assignments

--- vhdl/word_filler.vhd
+++ vhdl/word_filler.vhd
@@ -21,23 +21,26 @@ architecture BHV of word_filler is
   begin
 
   process(clk,rst)
-    variable byte_count : unsigned(3 downto 0) := 0;
+    variable byte_count : natural;
   begin
 
     if(rst = '1') then
+		word_wr_en <= '0';
+		word_out <= (others => '0');
+		byte_count := 0;
 
     elsif(rising_edge(clk))then
       word_wr_en <= '0';
+		word_out <= (others => '0');
 
       if(byte_wr_en = '1') then
-        word_out((23-(8*byte_count)) downto (8*(2-byte_count))) <= byte_in;
+        word_out((23-(8*byte_count)) downto (8*(2-byte_count))) <= byte_in; --((to_unsigned(23, 4)-(to_unsigned(8,4)*byte_count)) downto (to_unsigned(8,4)*(to_unsigned(2,4)-byte_count)))
         byte_count := byte_count +1;
       end if;
 
       if(byte_count = 3) then
         byte_count := 0;
         word_wr_en <= '1';
-
       end if;
 
     end if;
